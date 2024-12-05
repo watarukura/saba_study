c4a695b954bf26eaecfbedd480ee961b2304c84d
Wataru Kurashima <what.r.j150@gmail.com>
2024-12-05 13:55:50 +0900

    feat: ui_wasabi

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,24 @@
 extern crate alloc;
 
 use crate::alloc::string::ToString;
+use alloc::rc::Rc;
+use core::cell::RefCell;
 use noli::*;
 use saba_core::browser::Browser;
 use saba_core::http::HttpResponse;
-
-static TEST_HTTP_RESPONSE: &str = r#"HTTP/1.1 200 OK
-Data: xx xx xx
-
-<html>
-<head></head>
-<body>
-    <h1 id ="title">H1 title</h1>
-    <h2 class="class">H2 title</h2>
-    <p>
-        <a href="example.com">Link1</a>
-        <a href="example.com">Link2</a>
-    </p>
-</body>
-</html>
-"#;
+use ui_wasabi::app::WasabiUI;
 
 fn main() -> u64 {
     let browser = Browser::new();
 
-    let response =
-        HttpResponse::new(TEST_HTTP_RESPONSE.to_string()).expect("failed to parse http response");
-    let page = browser.borrow().current_page();
-    page.borrow_mut().receive_response(response);
+    let ui = Rc::new(RefCell::new(WasabiUI::new(browser)));
+    match ui.borrow_mut().start() {
+        Ok(_) => {}
+        Err(e) => {
+            println!("browser fails to start {:?}", e);
+            return 1;
+        }
+    }
 
     0
 }
---
+++ ui/wasabi/src/app.rs
@@ -0,0 +1,93 @@
+use alloc::format;
+use alloc::rc::Rc;
+use alloc::string::ToString;
+use core::cell::RefCell;
+use noli::error::Result as OsResult;
+use noli::window::StringSize;
+use noli::window::Window;
+use saba_core::browser::Browser;
+use saba_core::constants::*;
+use saba_core::error::Error;
+
+#[derive(Debug)]
+pub struct WasabiUI {
+    browser: Rc<RefCell<Browser>>,
+    window: Window,
+}
+
+impl WasabiUI {
+    pub fn new(browser: Rc<RefCell<Browser>>) -> Self {
+        Self {
+            browser,
+            window: Window::new(
+                "saba".to_string(),
+                WHITE,
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS,
+                WINDOW_WIDTH,
+                WINDOW_HEIGHT,
+            )
+            .unwrap(),
+        }
+    }
+
+    fn setup_toolbar(&mut self) -> OsResult<()> {
+        self.window
+            .fill_rect(LIGHTGREY, 0, 0, WINDOW_WIDTH, TOOLBAR_HEIGHT)?;
+
+        self.window
+            .draw_line(GREY, 0, TOOLBAR_HEIGHT, WINDOW_WIDTH - 1, TOOLBAR_HEIGHT)?;
+        self.window.draw_line(
+            DARKGREY,
+            0,
+            TOOLBAR_HEIGHT + 1,
+            WINDOW_WIDTH - 1,
+            TOOLBAR_HEIGHT + 1,
+        )?;
+
+        self.window.draw_string(
+            BLACK,
+            5,
+            5,
+            "Address:",
+            StringSize::Medium,
+            /*underline=*/ false,
+        )?;
+
+        self.window
+            .fill_rect(WHITE, 70, 2, WINDOW_WIDTH - 74, 2 + ADDRESSBAR_HEIGHT)?;
+
+        self.window.draw_line(GREY, 70, 2, WINDOW_WIDTH - 4, 2)?;
+        self.window
+            .draw_line(GREY, 70, 2, 70, 2 + ADDRESSBAR_HEIGHT)?;
+        self.window.draw_line(BLACK, 71, 3, WINDOW_WIDTH - 5, 3)?;
+
+        self.window
+            .draw_line(GREY, 71, 3, 71, 1 + ADDRESSBAR_HEIGHT)?;
+
+        Ok(())
+    }
+
+    fn setup(&mut self) -> Result<(), Error> {
+        if let Err(error) = self.setup_toolbar() {
+            return Err(Error::InvalidUI(format!(
+                "failed to initialize a toolbar with error: {:#?}",
+                error
+            )));
+        }
+        self.window.flush();
+        Ok(())
+    }
+
+    pub fn start(&mut self) -> Result<(), Error> {
+        self.setup()?;
+
+        self.run_app()?;
+
+        Ok(())
+    }
+
+    fn run_app(&mut self) -> Result<(), Error> {
+        Ok(())
+    }
+}
