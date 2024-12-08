55dd09a11e7e8b6c7ef3af550253359b8b3904b9
Wataru Kurashima <what.r.j150@gmail.com>
2024-12-08 10:06:40 +0900

    feat: chapter7 javascript

---
+++ saba_core/src/renderer/js/token.rs
@@ -0,0 +1,114 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum Token {
+    Punctuator(char),
+    Number(u64),
+}
+
+pub struct JsLexer {
+    pos: usize,
+    input: Vec<char>,
+}
+
+impl JsLexer {
+    pub fn new(js: String) -> Self {
+        Self {
+            pos: 0,
+            input: js.chars().collect(),
+        }
+    }
+
+    fn consume_number(&mut self) -> u64 {
+        let mut num = 0;
+
+        loop {
+            if self.pos >= self.input.len() {
+                return num;
+            }
+
+            let c = self.input[self.pos];
+
+            match c {
+                '0'..='9' => {
+                    num = num * 10 + (c.to_digit(10).unwrap() as u64);
+                    self.pos += 1;
+                }
+                _ => break,
+            }
+        }
+        return num;
+    }
+}
+
+impl Iterator for JsLexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+
+        // ホワイトスペース or 改行が続く限り、次の位置に進める
+        while self.input[self.pos] == ' ' || self.input[self.pos] == '\n' {
+            self.pos += 1;
+            if self.pos >= self.input.len() {
+                return None;
+            }
+        }
+
+        let c = self.input[self.pos];
+
+        let token = match c {
+            '+' | '-' | ';' | '=' | '(' | ')' | '{' | '}' | ',' | '.' => {
+                let t = Token::Punctuator(c);
+                self.pos += 1;
+                t
+            }
+            '0'..='9' => Token::Number(self.consume_number()),
+            _ => unimplemented!("char {:?} is not supported yet", c),
+        };
+
+        Some(token)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use alloc::string::ToString;
+
+    #[test]
+    fn test_empty() {
+        let input = "".to_string();
+        let mut lexer = JsLexer::new(input).peekable();
+        assert!(lexer.peek().is_none());
+    }
+
+    #[test]
+    fn test_num() {
+        let input = "42".to_string();
+        let mut lexer = JsLexer::new(input).peekable();
+        let expected = [Token::Number(42)].to_vec();
+        let mut i = 0;
+        while lexer.peek().is_some() {
+            assert_eq!(Some(expected[i]).clone(), lexer.next());
+            i += 1;
+        }
+        assert!(lexer.peek().is_none());
+    }
+
+    #[test]
+    fn test_add_nums() {
+        let input = "1 + 2".to_string();
+        let mut lexer = JsLexer::new(input).peekable();
+        let expected = [Token::Number(1), Token::Punctuator('+'), Token::Number(2)].to_vec();
+        let mut i = 0;
+        while lexer.peek().is_some() {
+            assert_eq!(Some(expected[i]).clone(), lexer.next());
+            i += 1;
+        }
+        assert!(lexer.peek().is_none());
+    }
+}
