96a07c6bef4796294c074bcd62e10e7e5200c52b
Wataru Kurashima <what.r.j150@gmail.com>
2024-12-09 14:08:43 +0900

    tests: add assign_variable

--- saba_core/src/renderer/js/runtime.rs
+++ saba_core/src/renderer/js/runtime.rs
@@ -1,39 +1,52 @@
 use crate::renderer::js::ast::{Node, Program};
+use alloc::format;
 use alloc::rc::Rc;
+use alloc::string::{String, ToString};
+use alloc::vec::Vec;
 use core::borrow::Borrow;
+use core::cell::RefCell;
+use core::fmt::{Display, Formatter};
 use core::ops::{Add, Sub};
 
-pub struct JsRuntime {}
+pub struct JsRuntime {
+    env: Rc<RefCell<Environment>>,
+}
 
 impl JsRuntime {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            env: Rc::new(RefCell::new(Environment::new(None))),
+        }
     }
 
     pub fn execute(&mut self, program: &Program) {
         for node in program.body() {
-            self.eval(&Some(node.clone()));
+            self.eval(&Some(node.clone()), self.env.clone());
         }
     }
 
-    fn eval(&mut self, node: &Option<Rc<Node>>) -> Option<RuntimeValue> {
+    fn eval(
+        &mut self,
+        node: &Option<Rc<Node>>,
+        env: Rc<RefCell<Environment>>,
+    ) -> Option<RuntimeValue> {
         let node = match node {
             Some(n) => n,
             None => return None,
         };
 
         match node.borrow() {
-            Node::ExpressionStatement(expr) => return self.eval(&expr),
+            Node::ExpressionStatement(expr) => return self.eval(&expr, env.clone()),
             Node::AdditiveExpression {
                 operator,
                 left,
                 right,
             } => {
-                let left_value = match self.eval(&left) {
+                let left_value = match self.eval(&left, env.clone()) {
                     Some(value) => value,
                     None => return None,
                 };
-                let right_value = match self.eval(&right) {
+                let right_value = match self.eval(&right, env.clone()) {
                     Some(value) => value,
                     None => return None,
                 };
@@ -47,16 +60,47 @@ impl JsRuntime {
                 }
             }
             Node::AssignmentExpression {
-                operator: _,
-                left: _,
-                right: _,
-            } => None,
+                operator,
+                left,
+                right,
+            } => {
+                if operator != &'=' {
+                    return None;
+                }
+                if let Some(node) = left {
+                    if let Node::Identifier(id) = node.borrow() {
+                        let new_value = self.eval(right, env.clone());
+                        env.borrow_mut().update_variable(id.to_string(), new_value);
+                        return None;
+                    }
+                }
+                None
+            }
             Node::MemberExpression {
                 object: _,
                 property: _,
             } => None,
             Node::NumericLiteral(value) => Some(RuntimeValue::Number(*value)),
-            _ => todo!(),
+            Node::VariableDeclaration { declarations } => {
+                for declaration in declarations {
+                    self.eval(&declaration, env.clone());
+                }
+                None
+            }
+            Node::VariableDeclarator { id, init } => {
+                if let Some(node) = id {
+                    if let Node::Identifier(id) = node.borrow() {
+                        let init = self.eval(&init, env.clone());
+                        env.borrow_mut().add_variable(id.to_string(), init);
+                    }
+                }
+                None
+            }
+            Node::Identifier(name) => match env.borrow_mut().get_variable(name.to_string()) {
+                Some(v) => Some(v),
+                None => Some(RuntimeValue::StringLiteral(name.to_string())),
+            },
+            Node::StringLiteral(value) => Some(RuntimeValue::StringLiteral(value.to_string())),
         }
     }
 }
@@ -64,14 +108,18 @@ impl JsRuntime {
 #[derive(Debug, Clone, PartialEq)]
 pub enum RuntimeValue {
     Number(u64),
+    StringLiteral(String),
 }
 
 impl Add<RuntimeValue> for RuntimeValue {
     type Output = RuntimeValue;
 
     fn add(self, rhs: RuntimeValue) -> RuntimeValue {
-        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
-        return RuntimeValue::Number(left_num + right_num);
+        if let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs) {
+            return RuntimeValue::Number(left_num + right_num);
+        }
+
+        RuntimeValue::StringLiteral(self.to_string() + &rhs.to_string())
     }
 }
 
@@ -79,8 +127,65 @@ impl Sub<RuntimeValue> for RuntimeValue {
     type Output = RuntimeValue;
 
     fn sub(self, rhs: RuntimeValue) -> RuntimeValue {
-        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
-        return RuntimeValue::Number(left_num - right_num);
+        if let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs) {
+            return RuntimeValue::Number(left_num - right_num);
+        }
+
+        // NaN: Not a number
+        RuntimeValue::Number(u64::MIN)
+    }
+}
+
+impl Display for RuntimeValue {
+    fn fmt(&self, f: &mut Formatter) -> core::fmt::Result {
+        let s = match self {
+            RuntimeValue::Number(value) => format!("{}", value),
+            RuntimeValue::StringLiteral(value) => value.to_string(),
+        };
+        write!(f, "{}", s)
+    }
+}
+
+type VariableMap = Vec<(String, Option<RuntimeValue>)>;
+#[derive(Debug, Clone)]
+pub struct Environment {
+    variables: VariableMap,
+    outer: Option<Rc<RefCell<Environment>>>,
+}
+
+impl Environment {
+    fn new(outer: Option<Rc<RefCell<Environment>>>) -> Self {
+        Self {
+            variables: VariableMap::new(),
+            outer,
+        }
+    }
+
+    pub fn get_variable(&self, name: String) -> Option<RuntimeValue> {
+        for variable in &self.variables {
+            if variable.0 == name {
+                return variable.1.clone();
+            }
+        }
+        if let Some(env) = &self.outer {
+            env.borrow_mut().get_variable(name)
+        } else {
+            None
+        }
+    }
+
+    fn add_variable(&mut self, name: String, value: Option<RuntimeValue>) {
+        self.variables.push((name, value));
+    }
+
+    fn update_variable(&mut self, name: String, value: Option<RuntimeValue>) {
+        for i in 0..self.variables.len() {
+            if self.variables[i].0 == name {
+                self.variables.remove(i);
+                self.variables.push((name, value));
+                return;
+            }
+        }
     }
 }
 
@@ -102,7 +207,7 @@ mod tests {
 
         let mut i = 0;
         for node in ast.body() {
-            let result = runtime.eval(&Some(node.clone()));
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
             assert_eq!(result, expected[i]);
             i += 1;
         }
@@ -119,7 +224,7 @@ mod tests {
 
         let mut i = 0;
         for node in ast.body() {
-            let result = runtime.eval(&Some(node.clone()));
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
             assert_eq!(result, expected[i]);
             i += 1;
         }
@@ -136,7 +241,24 @@ mod tests {
 
         let mut i = 0;
         for node in ast.body() {
-            let result = runtime.eval(&Some(node.clone()));
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
+            assert_eq!(result, expected[i]);
+            i += 1;
+        }
+    }
+
+    #[test]
+    fn test_assign_variable() {
+        let input = "var foo=42;".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [None];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
             assert_eq!(result, expected[i]);
             i += 1;
         }
